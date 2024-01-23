005bbce886f805f5bc4c87bc124a2fa66d912f9a
jayendramadaram <dimebeatengreen8@gmail.com>
2024-01-23 05:56:58 +0530

    Structure/project-files init

---
+++ Readme.md
@@ -0,0 +1,5 @@
+## run server and tests in watch mode
+$ cargo install cargo-watch
+
+cargo watch -q -c -w examples/ -x "run --example main"
+
---
+++ examples/main.rs
@@ -0,0 +1,7 @@
+// contains test examples for the server
+
+#![allow(unused)] // For test purposes!!
+
+fn main() {
+    print!("Hello, world!");
+}
